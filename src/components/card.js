057644f5382bac9e3888b688ab2cd29934f7e672
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-07-18 11:18:32 +0100

    designing card for my frontend project

--- src/components/card.js
+++ src/components/card.js
@@ -9,7 +9,12 @@ const Card = () => {
         "https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc"
       );
       const jsonData = await response.json();
-      const currentSubmit = jsonData.items.filter((item)=> new Date(item.created_at).toLocaleString("en-US", { day : '2-digit'}) < 30)
+      const currentSubmit = jsonData.items.filter(
+        (item) =>
+          new Date(item.created_at).toLocaleString("en-US", {
+            day: "2-digit",
+          }) < 30
+      );
       console.log(jsonData);
       setProfile(currentSubmit);
     } catch (error) {
@@ -31,26 +36,31 @@ const Card = () => {
           open_issues_count,
           owner,
           full_name,
-          created_at
+          created_at,
         } = data;
 
-     
         return (
           <>
             <div className="container" key={name}>
               <div>
                 {" "}
-                <img src={owner.avatar_url} alt={name}  />
+                <img src={owner.avatar_url} alt={name} />
               </div>
-              <div  className="text-container">
+              <div className="text-container">
                 {" "}
                 <p className="repo_name">{full_name}</p>
                 <p>{description}</p>
-               <div className="stargazers_count-container">
-               <p className="stars-container">Stars: {stargazers_count}</p>
-                <p className="stars-container">Issues: {open_issues_count}</p>
-                <p className="submitted">Submitted { new Date(created_at).toLocaleString("en-US", { day : '2-digit'})} days ago by {name}</p>
-               </div>
+                <div className="stargazers_count-container">
+                  <p className="stars-container">Stars: {stargazers_count}</p>
+                  <p className="stars-container">Issues: {open_issues_count}</p>
+                  <p className="submitted">
+                    Submitted{" "}
+                    {new Date(created_at).toLocaleString("en-US", {
+                      day: "2-digit",
+                    })}{" "}
+                    days ago by {name}
+                  </p>
+                </div>
               </div>
             </div>
           </>
@@ -59,6 +69,5 @@ const Card = () => {
     </div>
   );
 };
-
 
 export default Card;
